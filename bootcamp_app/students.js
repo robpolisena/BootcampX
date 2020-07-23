30714703e76edbc109103f8aacdac00a60f01bcc
Rob <robpolisena@gmail.com>
2020-07-23 00:30:03 +0000

    students.js

---
+++ bootcamp_app/students.js
@@ -0,0 +1,25 @@
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: 'vagrant',
+  password: '123',
+  host: 'localhost',
+  database: 'bootcampx'
+});
+
+
+const coName = process.argv[2]
+const limit = process.argv[3]
+
+pool.query(`
+SELECT students.id as student_id, students.name as name, cohorts.name as cohort
+FROM students
+JOIN cohorts ON cohort_id = cohorts.id
+WHERE cohorts.name LIKE '${coName}%'
+LIMIT ${limit};
+`)
+.then(res => {
+  res.rows.forEach(user => {
+    console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
+  })
+}).catch(err => console.error('query error', err.stack));
