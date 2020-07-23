0b44f119fa27f2e785c55bdcd9dc6181741427c3
Rob <robpolisena@gmail.com>
2020-07-23 00:33:25 +0000

    Javascript application to connect and query bootcampx database

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -8,8 +8,8 @@ const pool = new Pool({
 });
 
 
-const coName = process.argv[2]
-const limit = process.argv[3]
+const coName = process.argv[2];
+const limit = process.argv[3];
 
 pool.query(`
 SELECT students.id as student_id, students.name as name, cohorts.name as cohort
@@ -18,8 +18,8 @@ JOIN cohorts ON cohort_id = cohorts.id
 WHERE cohorts.name LIKE '${coName}%'
 LIMIT ${limit};
 `)
-.then(res => {
-  res.rows.forEach(user => {
-    console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
-  })
-}).catch(err => console.error('query error', err.stack));
+  .then(res => {
+    res.rows.forEach(user => {
+      console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
+    });
+  }).catch(err => console.error('query error', err.stack));
